6fa34f83d6a833e8cb2e857f61bdb65833d872ff
bishalprasad321 <bishalprasad321@gmail.com>
2022-10-16 02:32:40 +0530

    Created Splashscreen with lottie animation view

---
+++ app/src/main/java/com/bishal/tasktodo/SplashScreen.kt
@@ -0,0 +1,22 @@
+package com.bishal.tasktodo
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+@SuppressLint("CustomSplashScreen")
+@Suppress("DEPRECATION")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
